403c1fa2d21d8c481048458814476472feec0364
Lauren Yeager <Lauren.e.yeager@gmail.com>
2018-06-20 09:29:28 -0400

    Full Files and Directories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
- 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        { description: 'Walk the cat', isCompleted: true },
+        { description: 'Throw the dishes away', is Completed: false },
+        { description: 'Buy new dishes', isCompleted: false }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-        <ToDo />
-        <ToDo />
+        { this.state.todos.map( (todo, index) =>
+        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+      )}
         </ul>
       </div>
     );
