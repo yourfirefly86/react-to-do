2e81ac1d0dc27e734bf0cd342fa842b806572469
Lauren Yeager <Lauren.e.yeager@gmail.com>
2018-06-21 16:51:38 -0400

    Events Assignment

--- src/App.js
+++ src/App.js
@@ -33,12 +33,18 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
+  deleteToDo(index) {
+    const todos = this.state.todos.slice();
+    const todo = todos[index];
+    this.setState({ todos: todos.filter((item) => item !== todo) });
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteToDo={ () => this.deleteToDo(index) }/>
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
